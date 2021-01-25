44a33079d1c5ed3e54a760242885d00ef01c6eff
Arkadiusz Wiśniewski <syadnom.etah@gmail.com>
2021-01-25 17:27:18 +0100

    Added item stat popups and comparison

--- Project/PathOfExileEq/Assets/Scripts/Item.cs
+++ Project/PathOfExileEq/Assets/Scripts/Item.cs
@@ -8,11 +8,14 @@ using UnityEngine.UI;
 public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
 
-    [SerializeField] private Canvas _basicObcjet;
+    [SerializeField] private Canvas _basicObject;
     [SerializeField] private RectTransform _equipPosition;
     [SerializeField] private Camera _camera;
     [SerializeField] private Sprite[] _images;
-    public bool equip;
+    [SerializeField] private Image _stats;
+    [SerializeField] private Image _statsCompare;
+    [SerializeField] private Item _otherItem;
+    public bool equiped;
 
 
     public bool dragOnSurfaces = true;
@@ -32,31 +35,51 @@ public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     void Start()
     {
         initialPosition = transform.position;
-        equip = false;
+        equiped = false;
     }
 
     void Update()
     {
-
-        if (isOver && Input.GetMouseButtonUp(0) && Input.GetKey(KeyCode.LeftShift))
+        if (isOver)
         {
-            if (!equip)
-            {
-                transform.position = _equipPosition.position;
-                equip = true;
-                gameObject.GetComponent<Image>().sprite = _images[1];
-            }
-            else
-            {
-                transform.position = initialPosition;
-                equip = false;
-                gameObject.GetComponent<Image>().sprite = _images[0];
-            }
+            //Update item stats position
+            if(_otherItem != null && _otherItem.equiped)
+                _statsCompare.rectTransform.position = Input.mousePosition + new Vector3(10, 10);
+			else
+                _stats.rectTransform.position = Input.mousePosition + new Vector3(10, 10);
 
+            //Equip item
+            if (Input.GetMouseButtonUp(0) && Input.GetKey(KeyCode.LeftShift))
+			{
+				if (!equiped)
+				{
+                    if(_otherItem != null && _otherItem.equiped)
+                        _otherItem.Unequip();
+					Equip();
+				}
+				else
+				{
+					Unequip();
+				}
+			}
         }
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
+	public void Unequip()
+	{
+		transform.position = initialPosition;
+		equiped = false;
+		gameObject.GetComponent<Image>().sprite = _images[0];
+	}
+
+	public void Equip()
+	{
+		transform.position = _equipPosition.position;
+		equiped = true;
+		gameObject.GetComponent<Image>().sprite = _images[1];
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
     {
         canvasGroup.alpha = 0.6f;
         canvasGroup.blocksRaycasts = false;
@@ -65,7 +88,7 @@ public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta/_basicObcjet.scaleFactor;
+        rectTransform.anchoredPosition += eventData.delta/_basicObject.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -84,11 +107,21 @@ public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     {
         Debug.Log("Mouse enter");
         isOver = true;
+
+        if (_otherItem != null && _otherItem.equiped)
+            _statsCompare.gameObject.SetActive(true);
+        else
+            _stats.gameObject.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log("Mouse exit");
         isOver = false;
+
+        if (_statsCompare != null && _statsCompare.gameObject.activeSelf)
+            _statsCompare.gameObject.SetActive(false);
+        else
+            _stats.gameObject.SetActive(false);
     }
 }
